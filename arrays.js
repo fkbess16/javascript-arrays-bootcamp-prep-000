0303faf724c100dabda2525283b15a4afd28791f
Flannery Bessette <fkbess16@gmail.com>
2017-12-08 22:34:50 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,24 @@
+var chocolateBars  = ["snickers", "hundred grand", "kitkat", "skittles"];
+function destructivelyAddElementToBeginningOfArray (array,element) {array.unshift(element);
+return array;
+  }
+  function addElementToBeginningOfArray (array, element) { return [element,...array];
+  
+  }
+  function addElementToEndOfArray (array, element) { 
+    return [...array, element];
+  }
+  function destructivelyAddElementToEndOfArray (array, element){
+    array.push(element);
+    return array
+  }
+  function accessElementInArray (array,index) {
+    return (array[index]);
+  }
+  function destructivelyRemoveElementFromBeginningOfArray(array,element) {
+    array.shift();
+    return array}
+    function removeElementFromBeginningOfArray (array){
+      return array = array.slice(1);
+    }
+    
